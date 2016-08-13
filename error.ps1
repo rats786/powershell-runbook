57377c44b5a629acb748391153454b9b606f0f9f
rats786 <ratnankur.cool@Gmail.com>
2016-08-13 16:58:31 +0530

    Add files via upload

---
+++ error.ps1
@@ -0,0 +1,21 @@
+﻿#$Credential = Get-AutomationPSCredential -Name 'Ratz-powershell' 
+#$credential=Get-Credential
+  #Add-AzureRmAccount
+	#Login-AzureRmAccount
+   
+$sku = @{           'Name'='B2'
+					'Tier'='Basic'
+					'Size'='B2'
+					'Family'='B'
+					'Capacity'='2'                
+         }
+
+    $ASPResourceid=@("/subscriptions/bc53ebc2-813d-479b-a53e-0749c81129ac/resourceGroups/a-intelanalytics-eu-n-rwenp-rg/providers/microsoft.web/serverfarms/a-intelanalytics-eu-n-rwenp-wjs-asp")
+   foreach($Resourceid in $ASPResourceid)
+   {
+        $Resourceid
+       $ASPResourceObj = get-azurermresource -ResourceId $Resourceid 
+       $ASPResourceObj
+       set-AzureRmResource -ResourceId $Resourceid -sku $sku -Force
+    }
+   
